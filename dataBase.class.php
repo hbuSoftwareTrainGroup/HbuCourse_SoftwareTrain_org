347236184801e47f8e51ccc1ddeab0b5f402d4bd
summer <30340147+hiphopquan@users.noreply.github.com>
2019-01-14 20:40:41 +0800

    dataBase.class.php
    修改dataBase.class.php中的select方法

---
+++ dataBase.class.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 19597
+ * Date: 2019/1/14
+ * Time: 16:13
+ */
+
+class dataBase
+{
+    public function open(){
+        $link=@mysql_connect("localhost","root","root");
+        mysql_select_db("videorentalsystem",$link);
+        mysql_query("set names utf8");
+    }
+    public function select($sql){
+        $res=mysql_query($sql);
+		while($rows=mysql_fetch_array($res)){
+			$end[]=$rows;
+		}
+        return $end;
+    }
+    public function insert($sql){
+        $res=mysql_query($sql);
+        return $res;
+    }
+    public function update($sql){
+        $res=mysql_query($sql);
+        return $res;
+    }
+    public function dalete($sql){
+        $link=@mysql_connect("localhost","root","root");
+        mysql_select_db("shopping",$link);
+        mysql_query("set names utf8");
+    }
+}
